075cae51af0361711dfaf3d4b70edb4034c7bd46
perqualchebitinpiu <perqualchebitinpiu@gmail.com>
2018-01-15 23:22:52 +0100

    Add compiler

--- parte3/text_parser.py
+++ parte3/text_parser.py
@@ -3,8 +3,14 @@
 #Date: 	 18/01/2018
 #Description: semplice parser basato su pattern matching
 
+import urllib.request as urllib2
+
+    
 from textblob import TextBlob
 from fuzzywuzzy import fuzz
+from googlesearch.googlesearch import GoogleSearch
+from functools import partial
+
 
 #"Do you like ice-cream?"  -> "VBP,PRP,IN,NN"
 #Can you tell me about something?" -> "MD,PRP,VB,PRP,IN,NN"
@@ -34,7 +40,7 @@ cfg1.add_prod('NP', 'I')
 cfg1.add_prod('AUXV', 'DO | CAN')
 cfg1.add_prod('DET', 'THE | A')
 cfg1.add_prod('BE_VERB', 'IS | ARE')
-cfg1.add_prod('MAINV', 'BE_VERB | THINK | LIKE |FIND | SEARCH |HAVE | TELL')
+cfg1.add_prod('MAINV', 'BE_VERB | THINK | LIKE |FIND | SEARCH |HAVE | TELL |KNOW')
 cfg1.add_prod('WHW', 'WHAT | WHERE | WHEN')
 cfg1.add_prod('OBJ', 'CC NN| DET NN | DET NN CC OBJ')
 cfg1.add_prod('CC', ' OF')
@@ -46,26 +52,39 @@ def get_best_syntax_three(text, cfg):
     blob = TextBlob(text)
     ph_list = []
     #trova i token genera le regole semplificate
-    cfg.print_grammar()
-    print()
-    cfg_copy = CFG.CFG(cfg.prod)
-    cfg_copy.prune(blob.upper().words,1)
-    cfg_copy.print_grammar()
-    print()
-    #Genera un set di frasi 
-    for i in range(10): 
-        sent,tree = cfg_copy.gen_random_convergent('S')
-        ph_list.append(sent)
+    #Taglia l'albero a vari livelli fino e genera le frasi
+    #cfg.print_grammar()
+    #print()
+    for depth in range(4):
+        cfg_copy = CFG.CFG(cfg.prod)
+        cfg_copy.prune(blob.upper().words,depth)
+        if "S" in cfg_copy.prod:
+            #Genera un set di frasi 
+            for i in range(10): 
+                sent,tree = cfg_copy.gen_random_convergent('S')
+                ph_list.append({"S":sent,"Tree":tree,"Score":0})
+        else:
+            break
+                
     
-    print(ph_list)
     for ph in ph_list:
-        print(ph + str(fuzz.ratio(ph,blob.upper())))
+        ph["Score"] = fuzz.ratio(ph["S"],blob.upper())
     
+    max_score = ph_list[0]["Score"]
+    best_ph = ph_list[0]
+    for ph in ph_list:
+        if ph["Score"]> max_score:
+            max_score = ph["Score"]
+            best_ph  =  ph
+    
+    #print(best_ph)
+    return best_ph
 # sent,tree = cfg1.gen_random_convergent('S')
 # print()
 
 # print(sent)
 
+#Where does he live?
 
 # cfg1.prune(sent.split())
 
@@ -74,10 +93,66 @@ def get_best_syntax_three(text, cfg):
 
 # sent,tree = cfg1.gen_random_convergent('S')
 # print(sent)
-get_best_syntax_three("DO YOU LIKE  ICECREAM",cfg1)
 
 
+def greatings(name):
+    if (name): 
+        return ["hello: " + name]
+    else:
+        return ["hello"]
+        
+
+def search_for_info(something_to_search):
+    str_v =  ["I googled for " + str(something_to_search)]
+    str_v.append("I found this: .... ")
+    str_v.append("but I'm not smart enough to understand what it is, sorry")
+    return str_v
+
+def ask_for_info(something_unknown):
+    return ["I have  very limited vocabulary, I do not understand " + str(something_unknown)]
+
+def approve_res(state):
+    if state:
+        return ["yes"]
+    else:
+        return ["no"]
+    
+def you_want_to_know():
+    return ["You want to know"]
+
+    
+def parse_tree(tree):
+    #trova il tipodi frase e chiama la successiva funzione
+    PH= "."
+    for p in tree["Tree"]:
+        PH += p[0]+"."
+    print (PH)
+    actions = []
+    if PH == ".WHW.BE_VERB.OBJ.":
+         parse_WHW_BE_VERB_OBJ(tree["Tree"],actions)
+    else:
+        actions.append(partial(ask_for_info,something_to_search=tree["S"]))
+
+    return actions
+    
+def  parse_WHW_BE_VERB_OBJ(tree, action):
+    if tree[0][1][0][0] == "WHAT":
+        action.append(partial(you_want_to_know))
+        action.append(partial(search_for_info,something_to_search=tree[2][1][1]))
+
+    else:
+        action.append(partial(ask_for_info,something_to_search=tree[2][1][1]))
 
 
+best_ph = get_best_syntax_three("what is a cat",cfg1)
 
+print (best_ph)
+actions = parse_tree(best_ph)
 
+print(actions)
+
+res = []
+for a in actions:
+    res += a()
+    
+print (res)
